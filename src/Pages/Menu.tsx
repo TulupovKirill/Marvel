1482fae040beb1d90a96eda60e18efbbe1fe93bc
TulupovKirill <lolopo3457@gmail.com>
2024-07-04 22:14:46 +0600

    Do: Creating Menu

--- src/Pages/Menu.tsx
+++ src/Pages/Menu.tsx
@@ -1,57 +1,17 @@
-import { Link } from "react-router-dom"
 import './../styles/Menu.css'
 
 function Menu() {  
     return (
         <main>
-            <div className="menu_text">
-                <h1>Marvel Characters and Comics</h1>
-            </div>
-            
-            <div className="menu">
-                <div className="card_menu">
-                    <div className="card_menu_img_character">
-                        <img src="src/data/image/CharactersMarvel.png"/>
-                    </div>
+            <h1 className='menu_text'>Marvel Characters and Comics</h1>
+            <div className='menu'>
+                <div className="cards_menu">
+                    <img className='card_menu_characters' src="src/data/image/CharactersMarvel.png" height="100%" width="40%"/>
+                    <img className='card_menu_comics' src="src/data/image/ComicsMarvel.png" height="100%" width="40%"/>
                 </div>
-                <div className="card_menu">
-                    <div className="card_menu_img_comics">
-                        <img src="src/data/image/ComicsMarvel.png"/>
-                    </div>
-                </div>
-            </div>
-
-
-            <div className="menu">
-                <div className="card_menu">
-                    
-                    <div className="card_menu_describtion">
-                        <h2>Everything about Characters from the Marvel Universe</h2>
-                    </div>
-                </div>
-                <div className="card_menu">
-                    <div className="card_menu_describtion">
-                        <h2>Everything about Comics from the Marvel Universe</h2>
-                    </div>
-                </div>
-            </div>
-
-            <div className="menu">
-                <div className="card_menu">
-                    <nav className="card_menu_nav_button">
-                        <button>
-                            <Link to="/characters" className="card_menu_nav_button_text">Go to Characters</Link>
-                            {/* <a href="/characters" className="card_menu_nav_button_text">Go to Characters</a> */}
-                        </button>
-                    </nav>
-                </div>
-                <div className="card_menu">
-                    <nav className="card_menu_nav_button">
-                        <button>
-                            <Link to="/comics" className="card_menu_nav_button_text">Go to Comics</Link>
-                            {/* <a href="/comics" className="card_menu_nav_button_text">Go to Comics</a> */}
-                        </button>
-                    </nav>
+                <div className="cards_menu">
+                    <a className='card_menu_characters_link' href="/characters">Characters the Marvel Universe</a>
+                    <a className="card_menu_comics_link"href="/comics">Comics the Marvel Universe</a>
                 </div>
             </div>
         </main>
