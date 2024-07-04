2703b517879bb2b8adf0c636cf309b59d2525312
TulupovKirill <lolopo3457@gmail.com>
2024-07-04 13:58:12 +0600

    Do: Rename directory _Impl_ for _Pages_.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import type { RouteObject } from "react-router-dom";
 import { Outlet, Link, useRoutes, useParams } from "react-router-dom";
 
-import Header from './Impl/Header.tsx'
-import Footer from './Impl/Footer.tsx'
-import Menu from './Impl/Menu.tsx'
+import Header from './Pages/Header.tsx'
+import Footer from './Pages/Footer.tsx'
+import Menu from './Pages/Menu.tsx'
 
-import Character_Hulk from './Impl/Characters/Character_Hulk.tsx'
-import Character_IronMan from './Impl/Characters/Character_IronMan.tsx'
-import Character_SpiderMan from './Impl/Characters/Character_SpiderMan.tsx'
-import Screen_Characters from './Impl/Characters/Characters.tsx'
+import Character_Hulk from './Pages/Characters/Character_Hulk.tsx'
+import Character_IronMan from './Pages/Characters/Character_IronMan.tsx'
+import Character_SpiderMan from './Pages/Characters/Character_SpiderMan.tsx'
+import Screen_Characters from './Pages/Characters/Characters.tsx'
 
-import Comics_EssentialIronManVol1 from './Impl/Comics/Comics_EssentialIronManVol1.tsx'
-import Comics_HulkAngry from './Impl/Comics/Comics_HulkAngry.tsx'
-import Comics_ModernSpiderMan from './Impl/Comics/Comics_ModernSpiderMan.tsx'
-import Screen_Comics from './Impl/Comics/Comics.tsx'
+import Comics_EssentialIronManVol1 from './Pages/Comics/Comics_EssentialIronManVol1.tsx'
+import Comics_HulkAngry from './Pages/Comics/Comics_HulkAngry.tsx'
+import Comics_ModernSpiderMan from './Pages/Comics/Comics_ModernSpiderMan.tsx'
+import Screen_Comics from './Pages/Comics/Comics.tsx'
 
 
 function App() {
--- src/data/objects/Characters.tsx
+++ src/data/objects/Characters.tsx
@@ -2,19 +2,16 @@ export const characters = [
     {
         id: 2,
         name: 'Iron-Man',
-        url: '/characters/2',
         img: 'src/data/image/iron_man.png'
     },
     {
         id: 3,
         name: 'Spider-Man',
-        url: 'characters/2',
         img: 'src/data/image/spider_man.png'
     },
     {
         id: 1,
         name: 'Halk',
-        url: '/characters/1',
         img: 'src/data/image/hulk.png'
     }
 ];
