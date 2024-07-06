3dd4271ce1e774e1d3e2b92880a741ce2754b124
TulupovKirill <lolopo3457@gmail.com>
2024-07-06 13:03:42 +0600

    Stilisting Header. Add links to comics and charecters

--- src/Pages/Header.tsx
+++ src/Pages/Header.tsx
@@ -1,11 +1,20 @@
-import { Link } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import '../styles/Header.css'
 
 function Header() {  
     return (
         <header>
             <div className='MarvelLogo'>
-                <Link to="/">MARVEL</Link>
+                <div className='MarvelLogo_place_for_text'>
+                    <a className='MarvelLogo_text' href="/">MARVEL</a>
+                </div>
+                <div className='MarvelLogo_place_for_links'>
+                    <div className='MarvelLogo_links'>
+                        <a className='MarvelLogo_link_character' href="/Characters">Characters</a>
+                        <a className='MarvelLogo_link_comics' href="/Comics">Comics</a>
+                        <Outlet />
+                    </div>
+                </div>
             </div>
             
             <div className="Search">
